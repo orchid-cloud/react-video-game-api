47c858a7b82a5663f384cf0ff273eb19af81428e
NZ <n.prokopyk@gmail.com>
2024-06-20 12:37:27 +0200

    searching for games

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [gameTitle, setGameTitle] = useState('');
+
+  const searchGame = () => {
+    fetch(`https://www.cheapshark.com/api/1.0/games?title=${gameTitle}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
   return (
     <div className="App">
       <div className="searchSection">
         <h1>Search for a game</h1>
-        <input type="text" placeholder="Minecraft..." />
-        <button>Search game title</button>
+        <input
+          type="text"
+          placeholder="Minecraft..."
+          onChange={(event) => {
+            setGameTitle(event.target.value);
+          }}
+        />
+        <button onClick={searchGame}>Search game title</button>
       </div>
       <div className="dealsSection">
         <h2>Latest deals</h2>
