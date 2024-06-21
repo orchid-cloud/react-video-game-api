99b2c96b67aa7d070749b0ade8f56763f4998b13
NZ <n.prokopyk@gmail.com>
2024-06-21 11:17:36 +0200

    creating the deals section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
   const [gameTitle, setGameTitle] = useState('');
   const [searchedGames, setSearchedGames] = useState([]);
+  const [gameDeals, setGameDeals] = useState([]);
 
   const searchGame = () => {
     fetch(`https://www.cheapshark.com/api/1.0/games?title=${gameTitle}&limit=3`)
@@ -12,6 +13,17 @@ function App() {
         setSearchedGames(data);
       });
   };
+
+  useEffect(() => {
+    fetch(
+      `https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=20&pageSize=3`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setGameDeals(data);
+        console.log(data);
+      });
+  }, []);
   return (
     <div className="App">
       <div className="searchSection">
@@ -38,6 +50,9 @@ function App() {
       </div>
       <div className="dealsSection">
         <h2>Latest deals</h2>
+        {gameDeals.map((game, key) => {
+          return <h3 key={key}>{game.title}</h3>;
+        })}
       </div>
     </div>
   );
